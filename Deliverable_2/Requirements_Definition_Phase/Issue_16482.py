46b92bccb2b7c494acdb286d7c669e70c31fb12e
RiazCharania <riaz123456789@hotmail.com>
2020-03-06 23:04:30 -0500

    edited bug report code

--- Deliverable_2/Requirements_Definition_Phase/Issue_16482.py
+++ Deliverable_2/Requirements_Definition_Phase/Issue_16482.py
@@ -2,13 +2,13 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 initial_fig = plt.figure('initial')
-plt.hlines(0.5, 0, 1) # will add a horizontal line with the initial color in the color cycle blue
-plt.vlines(0.5, 0, 1, colors=None) # will add a vertical line with the initial default color black
+plt.hlines(0.5, 0, 1) # will add a horizontal line with the initial default color black
+plt.vlines(0.5, 0, 1, colors=None) # will add a vertical line with the initial color in the color cycle blue
 
 white_fig = plt.figure('white')
 with mpl.rc_context({'lines.color':'white'}): # sets the default line color to white
    plt.gca().set_facecolor('k') # sets background to black, for better visibility
-   plt.hlines(0.5, 0, 1) # incorrectly uses the initial default, black
+   plt.hlines(0.5, 0, 1) # still uses the initial default black
    plt.vlines(0.5, 0, 1, colors=None) # uses the new default white, as wanted.
 
 plt.show()
