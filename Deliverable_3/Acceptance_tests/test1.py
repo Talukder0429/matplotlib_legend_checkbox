9ff0066d727dc5ea346548c89cd50cefb02e1570
RiazCharania <riaz123456789@hotmail.com>
2020-03-20 11:46:18 -0400

    added acceptance tests

---
+++ Deliverable_3/Acceptance_tests/test1.py
@@ -0,0 +1,31 @@
+import numpy as np
+from matplotlib import patches
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+fig, ax = plt.subplots()
+
+# add a line
+x1 = 0
+x2 = 2
+y1 = 2
+line1 = ax.plot([x1,x2], [y1,y1], color='k', label='line1')
+
+# add a bar graph
+N = 3
+data = (1, 2, 3)
+width = .2
+barg = ax.bar(np.arange(N), data, width, color='b', label="bar")
+
+# add a circle
+x = 1
+y = 3
+r = .5
+circle = patches.Circle((x, y), r, color='r', label="circle")
+ax.add_patch(circle)
+
+# add the legend
+leg = ax.legend(checkbox=False)
+
+plt.show()
