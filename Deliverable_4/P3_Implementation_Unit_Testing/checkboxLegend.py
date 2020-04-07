09dbd94fafc4f57b66e1103b484f0fd59465d3d4
Arnob <arnob_talukder@hotmail.com>
2020-04-07 02:14:10 -0400

    updated checkboxLegend to no longer update default handeler. Added docstring

--- Deliverable_4/P3_Implementation_Unit_Testing/checkboxLegend.py
+++ Deliverable_4/P3_Implementation_Unit_Testing/checkboxLegend.py
@@ -2,95 +2,124 @@ import matplotlib.pyplot as plt
 from matplotlib.legend_handler import HandlerBase
 from matplotlib.transforms import Bbox, TransformedBbox
 from matplotlib.image import BboxImage
-from matplotlib.offsetbox import DrawingArea
 from matplotlib.legend import Legend
 from pathlib import Path
 
 
 class VisibilityHandler(HandlerBase):
+    """
+    A legend handler that shows checkboxes corresponding to handel
+    visibility in the legend entry.
+    """
+    # static checked and unchecked boxes
     _checked = plt.imread(Path.cwd() / "checked.png")
     _unchecked = plt.imread(Path.cwd() / "unchecked.png")
 
     def __init__(self, handler=None):
         # init attributes
         self.handler = handler
+        self.state = None
         super(VisibilityHandler, self).__init__()
 
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
-        # save original visibility, enable visible and set correct checkbox status
+        # shrink the gap between checkbox and label if needed
+        if (self.handler is None):
+            handlebox.set_width(handlebox.height)
+
+        return super(VisibilityHandler, self).legend_artist(legend, orig_handle, fontsize, handlebox)
+
+    def create_artists(self, legend, orig_handle, xdescent, ydescent, width, height, fontsize, trans):
+        # save original visibility, enable visible, and set correct checkbox status
         orig_vis = orig_handle.get_visible()
         orig_handle.set_visible(1)
         if not orig_vis:
             image_data = VisibilityHandler._unchecked
+            self.state = False
         else:
             image_data = VisibilityHandler._checked
+            self.state = True
+
+        # ratio for square checkbox
         image_ratio = image_data.shape[1] / image_data.shape[0]
 
-        # get dimensions
-        x0, y0 = handlebox.xdescent, handlebox.ydescent
-        width, height = handlebox.width, handlebox.height
-        trans = handlebox.get_transform()
-
-        # create and setup a checkbox image container, then add to handlebox
-        bb = Bbox.from_bounds(x0, y0, height * image_ratio, height)
+        # create a checkbox artist
+        bb = Bbox.from_bounds(xdescent, ydescent, height * image_ratio, height)
         tbb = TransformedBbox(bb, trans)
         image = BboxImage(tbb)
-
         image.set_data(image_data)
+
+        # update self
         self.update_prop(image, orig_handle, legend)
         self.set_events(image, orig_handle)
-        handlebox.add_artist(image)
 
-        # if a handler is given, create a container for it and add to handlebox
+        # artists to be returned
+        artists = [image]
+
+        # if a handler is given, create artists to be return
         if self.handler is not None:
-            da = DrawingArea(width / 2., height, x0 - width/2., y0)
-            artist = self.handler.legend_artist(
-                legend, orig_handle, fontsize, da)
-            artist.set_transform(trans)
-            handlebox.add_artist(da)
+            artists += self.handler.create_artists(
+                legend, orig_handle, xdescent - (height * 2.), ydescent, width - (height * 2.), height, fontsize, trans)
 
         # revert visibility
         orig_handle.set_visible(orig_vis)
-        return image
+        return artists
 
-    @classmethod
-    def set_events(cls, bbox, orig_handle):
+    def is_checked(self):
+        """
+        Returns true if checked, false otherwise.
+        """
+        return self.state
+
+    def set_events(self, bbox, orig_handle):
+        # swaps checkbox status
         def onclick(event):
             if event.button == 1 and bbox.contains(event)[0]:
                 if orig_handle.get_visible():
                     orig_handle.set_visible(False)
-                    bbox.set_data(cls._unchecked)
+                    bbox.set_data(VisibilityHandler._unchecked)
+                    self.state = False
                 else:
                     orig_handle.set_visible(True)
-                    bbox.set_data(cls._checked)
+                    bbox.set_data(VisibilityHandler._checked)
+                    self.state = True
                 plt.gcf().canvas.draw()
         plt.gcf().canvas.mpl_connect('button_press_event', onclick)
 
+        # matches checkbox status to visibility
         def ondraw(event):
             if orig_handle.get_visible():
-                bbox.set_data(cls._checked)
+                bbox.set_data(VisibilityHandler._checked)
+                self.state = True
             else:
-                bbox.set_data(cls._unchecked)
+                bbox.set_data(VisibilityHandler._unchecked)
+                self.state = False
         plt.gcf().canvas.mpl_connect('draw_event', ondraw)
 
 
-def checkboxLegend(parent, handles=None, labels=None, handler_map=None, handlelength=3, **kwargs):
-    # set update default handlers with given handlers
-    if handler_map is not None:
-        Legend.update_default_handler_map(handler_map)
+def checkboxLegend(parent, *args, **kwargs):
+    """
+    Creates a Legend with checkboxes to toggle visibility of all
+    elements corresponding to entries in the legend
 
-    # if no handles were given, fetch all legend handles
-    if handles is None:
-        ax = plt.gca()
-        handles, labels = ax.get_legend_handles_labels()
+    Parameters
+    ----------
+    parent : `~matplotlib.axes.Axes` or `.Figure`
+        The artist that contains the legend.
+    """
+    # get user genarated handler_map, and set handlelength to a default of 3
+    custom_handler_map = kwargs.get('handler_map', {})
+    if kwargs.get('handlelength', None) is None:
+        kwargs['handlelength'] = 3
 
-    # use our handler for given handles
-    hm = {}
-    for handle in handles:
-        default_handler = Legend.get_legend_handler(
-            Legend.get_default_handler_map(), handle)
-        hm[handle] = VisibilityHandler(handler=default_handler)
+    # update a default handler map to include user genarated handler_map
+    hm = Legend.get_default_handler_map().copy()
+    hm.update(custom_handler_map)
 
-    legend = parent.legend(handles, labels, handler_map=hm,
-                        handlelength=handlelength, **kwargs)
-    return legend
+    # create interactive version of the new handler map
+    for handler in hm:
+        hm[handler] = VisibilityHandler(
+            handler=Legend.get_legend_handler(hm, handler))
+    kwargs['handler_map'] = hm
+
+    # return interactive legend
+    return parent.legend(*args, **kwargs)
